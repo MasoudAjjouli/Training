d3c9b7ea37e2ff902c838f12fea600338bbee989
Masoud <Masoudzz@hotmail.com>
2022-02-28 15:16:08 +0200

    Tried Data Binding and fixed the shape of the code

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,43 +1,48 @@
 package com.example.myapp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.example.myapp.R.layout
+import androidx.databinding.DataBindingUtil
 import com.example.myapp.R.layout.*
+import com.example.myapp.databinding.ActivityMainBindingImpl
+import com.example.myapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(activity_main)
+        //setContentView(activity_main)
 
-       val button = findViewById<Button>(R.id.button)
-        val EditText = findViewById<EditText>(R.id.editTextTextEmailAddress)
-        val EditPassword = findViewById<EditText>(R.id.editTextTextPassword)
-        val str: String = EditText.text.toString()
-        val str2: String = EditPassword.text.toString()
-        button.setOnClickListener{
+        var binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-            if (EditText.text.isNullOrBlank() && EditPassword.text.isNullOrBlank()){
-                Toast.makeText(this,"Please Enter your Email and Passowrd",Toast.LENGTH_SHORT).show()
+        binding.myVar = ("Hi")
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val editPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val str: String = editText.text.toString()
+        val str2: String = editPassword.text.toString()
+        button.setOnClickListener {
+
+            if (editText.text.isNullOrBlank() && editPassword.text.isNullOrBlank()) {
+                Toast.makeText(this, "Please Enter your Email and Passowrd", Toast.LENGTH_SHORT)
+                    .show()
             }
 
-            if (EditText.text.isNotBlank() && EditPassword.text.isNullOrBlank()){
-                Toast.makeText(this,"Please Enter your Passowrd",Toast.LENGTH_SHORT).show()
+            if (editText.text.isNotBlank() && editPassword.text.isNullOrBlank()) {
+                Toast.makeText(this, "Please Enter your Passowrd", Toast.LENGTH_SHORT).show()
             }
 
-            if (EditPassword.text.isNotBlank() && EditText.text.isNullOrBlank()){
-                Toast.makeText(this,"Please Enter your Email",Toast.LENGTH_SHORT).show()
+            if (editPassword.text.isNotBlank() && editText.text.isNullOrBlank()) {
+                Toast.makeText(this, "Please Enter your Email", Toast.LENGTH_SHORT).show()
             }
-            if (EditText.text.isNotBlank() && EditPassword.text.isNotBlank()){
-                Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show()
+            if (editText.text.isNotBlank() && editPassword.text.isNotBlank()) {
+                Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
             }
-
 
 
         }
