eaa3df15a494178d1b409288553e6858065fd1c9
Masoud <Masoudzz@hotmail.com>
2022-03-06 13:54:03 +0200

    Applied data binding fully! and added the email validation

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.myapp
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -11,46 +12,57 @@ import com.example.myapp.R.layout.*
 import com.example.myapp.databinding.ActivityMainBindingImpl
 import com.example.myapp.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //setContentView(activity_main)
 
-        var binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        var binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.myVar = ("Hi")
-        val button = findViewById<Button>(R.id.button)
-        val editText = findViewById<EditText>(R.id.editTextTextEmailAddress)
-        val editPassword = findViewById<EditText>(R.id.editTextTextPassword)
-        val str: String = editText.text.toString()
-        val str2: String = editPassword.text.toString()
-        button.setOnClickListener {
+        binding.myVar = ("Welcome Back!")
 
-            if (editText.text.isNullOrBlank() && editPassword.text.isNullOrBlank()) {
+        binding.login.setOnClickListener {
+
+            binding.emailAddress.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.emailAddress.text.toString())
+                            .matches()
+                    )
+                        binding.login.isEnabled = true
+                    else {
+                        binding.login.isEnabled = false
+                        binding.emailAddress.setError("Invalid Email Address!")
+                    }
+                }
+            })
+
+            if (binding.emailAddress.text.isNullOrBlank() && binding.password.text.isNullOrBlank())
                 Toast.makeText(this, "Please Enter your Email and Passowrd", Toast.LENGTH_SHORT)
                     .show()
+
+            if (binding.emailAddress.text.isNotBlank() && binding.password.text.isNullOrBlank()) {
+
+                Toast.makeText(this, "Please Enter your Password", Toast.LENGTH_SHORT).show()
             }
 
-            if (editText.text.isNotBlank() && editPassword.text.isNullOrBlank()) {
-                Toast.makeText(this, "Please Enter your Passowrd", Toast.LENGTH_SHORT).show()
-            }
+            if (binding.emailAddress.text.isNullOrBlank() && binding.password.text.isNotBlank()) {
 
-            if (editPassword.text.isNotBlank() && editText.text.isNullOrBlank()) {
                 Toast.makeText(this, "Please Enter your Email", Toast.LENGTH_SHORT).show()
             }
-            if (editText.text.isNotBlank() && editPassword.text.isNotBlank()) {
+
+            if (binding.emailAddress.text.isNotBlank() && binding.password.text.isNotBlank()) {
                 Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
             }
 
-
         }
-
-
-        println("the value is $str ")
-        println("the value is $str2")
-
     }
-
 }
+
