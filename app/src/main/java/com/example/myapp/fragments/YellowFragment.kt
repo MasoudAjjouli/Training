6399deb826cafe4cb425153aa3b34bfe6ca462cc
Masoud <Masoudzz@hotmail.com>
2022-04-21 17:28:41 +0300

    Added User details fragment

--- app/src/main/java/com/example/myapp/fragments/YellowFragment.kt
+++ app/src/main/java/com/example/myapp/fragments/YellowFragment.kt
@@ -1,6 +1,7 @@
 package com.example.myapp.fragments
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,32 +9,39 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp.R
 import com.example.myapp.RecycleViewAdapter
 import com.example.myapp.UserClickListener
+import com.example.myapp.databinding.RecyclerListRowBinding
 import com.example.myapp.databinding.YellowFragmentBinding
 import com.example.myapp.models.UserInfoElement
 import com.example.myapp.viewModel.WelcomeViewModel
 
-
-class YellowFragment : Fragment() , UserClickListener{
+class YellowFragment : Fragment(), UserClickListener {
     private lateinit var binding: YellowFragmentBinding
     private lateinit var recyclerAdapter: RecycleViewAdapter
-    private val viewModel by lazy {  ViewModelProvider(this).get(WelcomeViewModel::class.java)}
+    private val viewModel by lazy { ViewModelProvider(this).get(WelcomeViewModel::class.java) }
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-         binding = DataBindingUtil.inflate<YellowFragmentBinding>(inflater,
-            R.layout.yellow_fragment,container,false)
+        binding = DataBindingUtil.inflate<YellowFragmentBinding>(
+            inflater,
+            R.layout.yellow_fragment, container, false
+        )
         initView()
         initeViewModel()
         return binding.root
 
     }
-
     private fun initView() {
 
         val yellowFragment = this
@@ -41,12 +49,11 @@ class YellowFragment : Fragment() , UserClickListener{
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = RecycleViewAdapter(this)
-
-
         recyclerView.adapter = recyclerAdapter
+
     }
 
-    private fun initeViewModel(){
+    private fun initeViewModel() {
         viewModel.getRecyclerListObserver().observe(viewLifecycleOwner) {
             it?.let {
                 recyclerAdapter.setUpdatedData(it)
@@ -54,23 +61,42 @@ class YellowFragment : Fragment() , UserClickListener{
         }
         viewModel.makeApiCall()
 
-
-
     }
-    override fun onClick (User: UserInfoElement) {
 
+    override fun onClick(User: UserInfoElement) {
 
-        TODO("navigate to new fragment using navigation components " +
-            "send the user details to the new fragment" +
-            "name the fragment - User details" +
-            "display the details in the new fragment")
+        val action = BaseFragmentDirections.actionBaseFragmentToUserDetails(User.fullName,User.emailAddress)
+        findNavController().navigate(action)
 
     }
 
     companion object {
-        fun newInstance()= YellowFragment()
+        fun newInstance() = YellowFragment()
     }
 
-
 }
+
+
+
+// val bundle = Bundle()
+//        bundle.putString("data",User.fullName)
+//        bundle.putString("data2",User.emailAddress)
+//        val fragment = UserDetails()
+//        fragment.arguments = bundle
+
+//       // val userFragment = UserDetails()
+////        val fragment = UserDetails().newInstance(User.fullName)
+////        val fragment2 = UserDetails().newInstance(User.emailAddress)
+//      //  val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+////        transaction.replace(R.id.baseFragment,UserDetails())
+////        transaction.commit()
+//
+
+//        fragmentManager?.beginTransaction()?.replace(R.id.baseFrag,fragment)?.commit()
+//
+////        TODO("navigate to new fragment using navigation components " +
+////            "send the user details to the new fragment" +
+////            "name the fragment - User details" +
+////            "display the details in the new fragment")
+//
 
