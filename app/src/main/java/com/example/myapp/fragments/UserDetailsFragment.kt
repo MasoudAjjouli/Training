d3e19f96866d751e81e2681df65a96bd0cd96e8a
Masoud <Masoudzz@hotmail.com>
2022-05-11 23:16:18 +0300

    Added hilt

--- app/src/main/java/com/example/myapp/fragments/UserDetailsFragment.kt
+++ app/src/main/java/com/example/myapp/fragments/UserDetailsFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.myapp.R
+import com.example.myapp.UserDetailsViewModel
 import com.example.myapp.databinding.UserDetailsFragmentBinding
-import com.example.myapp.databinding.YellowFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UserDetailsFragment : Fragment() {
 
     private val args: UserDetailsFragmentArgs by navArgs()
     private lateinit var binding: UserDetailsFragmentBinding
+    private val viewModel :UserDetailsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,13 +33,20 @@ class UserDetailsFragment : Fragment() {
             false
         )
 
+        viewModel
         initView()
+
         return binding.root
 
     }
 
-
     private fun initView() {
+
+        binding.apply {
+            fullName.text = viewModel.taskName
+            emailAddress.text = viewModel.taskEmail
+            bio.text = viewModel.taskBio
+        }
 
 //        binding.fullName.text = args.currentUser?.fullName
 //        binding.emailAddress.text = args.currentUser?.emailAddress
